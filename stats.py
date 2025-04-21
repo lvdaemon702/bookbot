27b2b6ae05d335e4042cf110629e305c4de6bce9
LVDaemon702 <lvnexusx@gmail.com>
2025-04-21 09:12:59 -0700

    Still need to learn more

---
+++ stats.py
@@ -0,0 +1,25 @@
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered.isalpha():
+            if lowered in chars:
+                chars[lowered] += 1
+            else:
+                chars[lowered] = 1
+    return chars
+
+def sort_on(d):
+    return d["count"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list=[]
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "count": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
